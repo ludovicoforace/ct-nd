2d36e0bbe7eb659a7e51c640a6789d069212e1ff
Ludovico Forace <ludovicoforace@yahoo.com>
2021-09-25 18:48:56 +0100

    Add constants and types

---
+++ src/constants.ts
@@ -0,0 +1,15 @@
+import { CardinalDirection } from "./types"
+
+export const clockwiseSeq = [
+  CardinalDirection.North,
+  CardinalDirection.East,
+  CardinalDirection.South,
+  CardinalDirection.West
+]
+
+export const antiClockwiseSeq = [
+  CardinalDirection.North,
+  CardinalDirection.West,
+  CardinalDirection.South,
+  CardinalDirection.East
+]
---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export enum CardinalDirection {
+  North = 'N',
+  South = 'S',
+  West = 'W',
+  East = 'E'
+}
+
+export enum Rotation {
+  Left = 'L',
+  Right = 'R'
+}
